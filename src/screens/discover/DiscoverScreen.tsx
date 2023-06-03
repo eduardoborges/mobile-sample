c1ff0ae27d25d2937bdac9f4493270d3c561c616
Eduardo Borges <euduardoborges@gmail.com>
2023-06-03 19:32:12 -0300

    style: improve component layout in DiscoverScreen

--- src/screens/discover/DiscoverScreen.tsx
+++ src/screens/discover/DiscoverScreen.tsx
@@ -1,14 +1,27 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Container } from '~/components';
+import {
+  Container, Column, Columns, Typography,
+} from '~/components';
+import { date, range } from '~/utils';
 
 export default function DiscoverScreen() {
   const navigation = useNavigation();
   // navigation.navigate('DISCOVER_SCREEN');
 
   return (
-    <Container title="Discover">
-      {/*  */}
+    <Container title={`Olá ${date().format('ll')}`}>
+      {range(1, 100).map((item) => (
+        <Columns key={item}>
+          <Column>
+            <Typography.H1>
+              {item}
+              {' '}
+              Eduardo Borges AAAA
+            </Typography.H1>
+          </Column>
+        </Columns>
+      ))}
     </Container>
   );
 }
