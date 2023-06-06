6aa7ca940e68b51bf1bf64b59591f83c9f3c135f
Eduardo Borges <euduardoborges@gmail.com>
2023-06-05 22:46:07 -0300

    feat: Add store list and product cards to Discover screen

--- src/screens/discover/DiscoverScreen.tsx
+++ src/screens/discover/DiscoverScreen.tsx
@@ -1,27 +1,77 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { FlatList, Image, ScrollView } from 'react-native';
+import { faker } from '@faker-js/faker';
 import {
-  Container, Column, Columns, Typography,
+  Container, Column, Columns, Card, Heading, P,
 } from '~/components';
-import { date, range } from '~/utils';
+import { formatBRL, range } from '~/utils';
+import { theme } from '~/theme';
 
 export default function DiscoverScreen() {
-  const navigation = useNavigation();
-  // navigation.navigate('DISCOVER_SCREEN');
 
   return (
-    <Container title={`Olá ${date().format('ll')}`}>
-      {range(1, 100).map((item) => (
-        <Columns key={item}>
-          <Column>
-            <Typography.H1>
-              {item}
-              {' '}
-              Eduardo Borges AAAA
-            </Typography.H1>
-          </Column>
+    <Container title="Olá">
+      <Heading size={2}>Lojas Próximas</Heading>
+
+      <ScrollView horizontal snapToInterval={250}>
+        <Columns>
+          {range(1, 20).map((item) => (
+            <Column m={5}>
+              <Card>
+                <Image
+                  source={{
+                    uri: `https://picsum.photos/500/500/${item}`,
+                  }}
+                  style={{
+                    borderRadius: 10,
+                    width: 300,
+                    height: 400,
+                    resizeMode: 'cover',
+                  }}
+                />
+              </Card>
+            </Column>
+          ))}
         </Columns>
-      ))}
+      </ScrollView>
+
+      {/*  */}
+      <Columns multiline>
+        <FlatList
+          numColumns={2}
+          data={range(1, 200)}
+          renderItem={({ item }) => (
+            <Column width="50%" p={5} key={item}>
+              <Card>
+                <Image
+                  source={{
+                    uri: 'https://picsum.photos/200/150',
+                  }}
+                  style={{
+                    width: '100%',
+                    aspectRatio: 5 / 4,
+                    resizeMode: 'cover',
+                    borderTopLeftRadius: 5,
+                    borderTopRightRadius: 5,
+                  }}
+                />
+                <Columns multiline>
+                  <Column p={[10, 10]}>
+                    <Heading size={4} color={theme.colors.success}>
+                      {formatBRL(faker.commerce.price())}
+                    </Heading>
+                  </Column>
+                  <Column p={[0, 0, 10, 10]}>
+                    <P size="s">
+                      {faker.commerce.productName()}
+                    </P>
+                  </Column>
+                </Columns>
+              </Card>
+            </Column>
+          )}
+        />
+      </Columns>
     </Container>
   );
 }
