635cb5f8d2e6c212bf8c7faeea4305e8911d899e
Eduardo Borges <euduardoborges@gmail.com>
2023-06-05 22:45:27 -0300

    refactor: simplify Card component and styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
-import styles from './Card.styles';
-import { CardProps } from './Card.types';
+import React, { useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export function Card(props: CardProps) {
-  const { children, ...rest } = props;
-  const s = styles(props);
+export type CardProps = React.PropsWithChildren<{
+  debug?: boolean;
+}>;
+
+export function Card({ children, ...props }: CardProps) {
+  const s = useMemo(() => getStyles(props), [props]);
+
   return (
     <View style={s.container}>
       {children}
     </View>
   );
 }
+
+const getStyles = (p: CardProps) => StyleSheet.create({
+  container: {
+    backgroundColor: '#fefefe',
+    borderRadius: 10,
+    borderColor: '#F2f2f2',
+    width: '100%',
+    minHeight: 50,
+    borderWidth: 1,
+    ...p.debug && {
+      //
+    },
+  },
+});
--- src/components/Card/Card.types.ts
+++
@@ -1,4 +0,0 @@
-export type CardProps = {
-  title: string;
-  children: React.ReactNode;
-};
