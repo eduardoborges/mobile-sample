09c4391fa0de0080da97f48181ff3e50e852965e
Eduardo Borges <euduardoborges@gmail.com>
2023-08-04 15:33:39 -0300

    chore(ios): update build scripts

--- scripts.sh
+++ scripts.sh
@@ -26,17 +26,24 @@ function ios(){
 
 function build-ios(){
   cd ios;
-  xcodebuild -workspace "MyApp.xcworkspace" \
+
+  xcrun xcodebuild archive \
+    -workspace "MyApp.xcworkspace" \
     -scheme "MyApp" \
     -configuration Release \
     -sdk iphoneos \
-    -derivedDataPath ./build \
+    -derivedDataPath ./build/DerivedData \
+    -archivePath ./build/MyApp.xcarchive \
     -allowProvisioningUpdates \
     -allowProvisioningDeviceRegistration;
 
-  xcodebuild -exportArchive -archivePath build/MyApp.xcarchive \
-    -exportOptionsPlist exportOptions.plist \
-    -exportPath build;
+  xcrun xcodebuild \
+    -exportArchive \
+    -exportOptionsPlist ExportOptions.plist \
+    -archivePath ./build/MyApp.xcarchive \
+    -exportPath ./build/MyApp.ipa \
+    -allowProvisioningUpdates \
+    -allowProvisioningDeviceRegistration;
 }
 
 function build-android(){
@@ -56,6 +63,14 @@ function lint(){
 
 function test(){
   jest src;
+}
+
+function postinstall(){
+  # macOS only
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    npx react-native setup-ios-permissions
+    cd ios && pod install && cd ..;
+  fi
 }
 
 func=$1
