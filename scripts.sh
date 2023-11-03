44d62e7ba677d33480e7250481c8b6184483ea11
Eduardo Borges <euduardoborges@gmail.com>
2023-11-03 11:42:23 -0300

    chore: update CI and CD

--- scripts.sh
+++ scripts.sh
@@ -65,6 +65,35 @@ function ios-export(){
     -allowProvisioningDeviceRegistration;
 }
 
+function setup-ios(){
+  # create variables
+  CERTIFICATE_PATH=$RUNNER_TEMP/build_certificate.p12
+  DEV_CERTIFICATE_PATH=$RUNNER_TEMP/build_dev_certificate.p12
+  PP_PATH=$RUNNER_TEMP/build_pp.mobileprovision
+  KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
+
+  # import certificate and provisioning profile from secrets
+  echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
+  echo -n "$BUILD_DEV_CERTIFICATE_BASE64" | base64 --decode -o $DEV_CERTIFICATE_PATH
+  echo -n "$BUILD_PROVISION_PROFILE_BASE64" | base64 --decode -o $PP_PATH
+
+
+  # create temporary keychain
+  security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
+  security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
+  security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
+
+  # import certificate to keychain
+  security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+  security list-keychain -d user -s $KEYCHAIN_PATH
+  security import $DEV_CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+  security list-keychain -d user -s $KEYCHAIN_PATH
+
+  # apply provisioning profile
+  mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+  cp $PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
+}
+
 function ios-build(){
   echo "📱 Building iOS...";
   ios-archive;
