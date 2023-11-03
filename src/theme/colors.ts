3ade2c96019232b76cddc26a78711d3cf6cd94f0
Eduardo Borges <euduardoborges@gmail.com>
2023-11-03 15:51:34 -0300

    Add new gray color shades to theme

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,3 +1,8 @@
 export const primary = '#007bff';
 
 export const success = 'hsl(153, 53%, 45%)';
+
+export const grayLight = 'hsl(210, 36%, 90%)';
+export const gray = 'hsl(210, 22%, 80%)';
+export const grayDark = 'hsl(210, 22%, 40%)';
+export const grayDarker = 'hsl(210, 22%, 10%)';
